1044dd1d1ffba70932aa9a95fc26a00085cadb09
Joel Prebish <prebish.j@outlook.com>
2024-09-03 15:58:29 -0400

    added finance section view

--- Finance Me/FinanceView.swift
+++ Finance Me/FinanceView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct FinanceView: View {
+
+    // States of things
+    // this is just filler comments
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            Form {
+                Section(header: Text("Accounts")) {
+                    NavigationLink(destination: AccountDetailView()) {
+                        Text("Account 1")
+                    }
+                    NavigationLink(destination: AccountDetailView()) {
+                        Text("Account 2")
+                    }
+                    NavigationLink(destination: AccountDetailView()) {
+                        Text("Account 3")
+                    }
+                }
+                Section(header: Text("Savings")) {
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Savings 1")
+                    }
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Savings 2")
+                    }
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Savings 3")
+                    }
+                }
+                Section(header: Text("Subscriptions")) {
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Subscription 1")
+                    }
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Subscription 2")
+                    }
+                    NavigationLink(destination: SavingsDetailView()) {
+                        Text("Subscription 3")
+                    
+                    }
+
+                }
+            }
+        }
+        .navigationTitle("Finance")
     }
 }
 
